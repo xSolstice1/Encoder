6c06befc3d6fd2f00430ae00340619e26ed12691
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2023-10-26 20:09:48 +0800

    Encoder init

---
+++ Encoder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Encoder {
+    public String encode(String plainText) {
+        return "";
+    }
+
+    public String decode(String encodedText) {
+        return "";
+    }
+}
